d9fb7e83e23bafb99fb98f7357fb6b7d46326816
lucanata <luca.natali@student.unife.it>
2014-10-28 18:39:08 +0100

    Login quasi ok

---
+++ app/views/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('content')
+
+	<div class="welcome">
+	@if (Session::has('flash_error'))
+		<div id="flash_error">{{ Session::get('flash_error') }}</div>
+	@endif
+
+		{{ Form::open(array('url' => 'login')) }}
+			<h1>Log in</h1>
+		
+
+			<p>
+				{{ Form::label('username', 'Username') }}<br>
+				{{ Form::text('username', Input::old('username')) }}
+			</p>
+			<p>
+				{{ Form::label('password', 'Password') }}<br>
+				{{ Form::password('password') }}
+			</p>
+
+			<p>
+				{{ Form::submit('Login') }}
+			</p>
+
+		{{ Form::close() }}
+	</div>
+@stop
